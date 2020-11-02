1055d056914e94ce41b19d10f22949390762af7f
Pascal Dziersk <p.dziersk@googlemail.com>
2020-11-02 11:31:49 +0100

    Remove left & right white bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -43,14 +43,6 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
-    <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
