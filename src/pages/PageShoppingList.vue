f04e5e07a727f748a362151d207386e71b2783b0
Pascal Dziersk <p.dziersk@googlemail.com>
2020-11-03 21:58:03 +0100

    Adjusted Shoppinglist

--- src/pages/PageShoppingList.vue
+++ src/pages/PageShoppingList.vue
@@ -1,44 +1,53 @@
 <template>
-  <q-page class="flex flex-top-left">
-    <q-list class="q-pl-xl">
+  <q-page class="row justify-center q-pa-md">
+    <q-list>
       <q-list-header><h3>Einkaufsliste</h3></q-list-header>
-      <q-item>
-        <q-item class="q-px-xl">
-          <q-item-section>
-            <q-item-label><h6>Bananen</h6></q-item-label>
-            <q-item-label caption>350g</q-item-label>
-          </q-item-section>
+      <q-item class="q-px-xl q-mx-xl">
+        <q-item-section class="q-pr-xl">
+          <q-item-label class="text-body1">Bananen</q-item-label>
+          <q-item-label caption>350g</q-item-label>
+        </q-item-section>
 
-          <q-item-section side bottom>
-            <q-item-label caption>95 kcal pro 100g</q-item-label>
-            <q-icon name="local_fire_department" color="orange" />
-          </q-item-section>
-        </q-item>
+        <q-item-section class="q-px-xl">
+          <!--spacer-->
+          <!--q-icon name="local_fire_department" class="inline" color="orange" /-->
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label caption>95 kcal pro 100g</q-item-label>
+        </q-item-section>
       </q-item>
-      <q-item>
-        <q-item-main>
-          <q-item-tile label>Apfel</q-item-tile>
-        </q-item-main>
+
+      <q-item class="q-px-xl q-mx-xl">
+        <q-item-section class="q-pr-xl">
+          <q-item-label class="text-body1">Nutella</q-item-label>
+          <q-item-label caption>1 Glas</q-item-label>
+        </q-item-section>
+
+        <q-item-section class="q-px-xl">
+          <!--spacer-->
+          <!--q-icon name="local_fire_department" class="inline" color="orange" /-->
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label caption>541 kcal pro 100g</q-item-label>
+        </q-item-section>
       </q-item>
-      <q-item>
-        <q-item-main>
-          <q-item-tile label>Reis</q-item-tile>
-        </q-item-main>
-      </q-item>
-      <q-item>
-        <q-item-main>
-          <q-item-tile label>Hühnchen</q-item-tile>
-        </q-item-main>
-      </q-item>
-      <q-item>
-        <q-item-main>
-          <q-item-tile label>Nutella</q-item-tile>
-        </q-item-main>
-      </q-item>
-      <q-item>
-        <q-item-main>
-          <q-item-tile label>Rinder Riegel</q-item-tile>
-        </q-item-main>
+
+      <q-item class="q-px-xl q-mx-xl">
+        <q-item-section class="q-pr-xl">
+          <q-item-label class="text-body1">Apfelmus</q-item-label>
+          <q-item-label caption>1 Glas</q-item-label>
+        </q-item-section>
+
+        <q-item-section class="q-px-xl">
+          <!--spacer-->
+          <!--q-icon name="local_fire_department" class="inline" color="orange" /-->
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label caption>68 kcal pro 100g</q-item-label>
+        </q-item-section>
       </q-item>
     </q-list>
   </q-page>
