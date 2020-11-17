07164acd827e6555a87263af0a438ec14b7e11f1
Pascal Dziersk <p.dziersk@googlemail.com>
2020-11-17 11:29:45 +0100

    changed qenv conf

--- src/boot/serverConnection.js
+++ src/boot/serverConnection.js
@@ -1,15 +1,7 @@
 import firebaseService from '../services/firebase'
 
 export default async () => {
-  const config = {
-    "apiKey": "AIzaSyD2d7_cMhVANtJdT7ec0qUlh6oWlDF4xs8",
-      "authDomain": "meal-o-mat-calendar.firebaseapp.com",
-      "databaseURL": "https://meal-o-mat-calendar.firebaseio.com",
-      "projectId": "meal-o-mat-calendar",
-      "storageBucket": "meal-o-mat-calendar.appspot.com",
-      "messagingSenderId": "221271990775",
-      "appId": "1:221271990775:web:15125ed2bab083f3328ef2"
-  };
+  const config = process.env.environments.FIREBASE_CONFIG
   firebaseService.fBInit(config)
 
   // Validation that our service structure is working
