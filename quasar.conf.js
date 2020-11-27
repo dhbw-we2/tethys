cd972bc4fe680970b36156927d23bf99ba8c71da
Pascal Dziersk <p.dziersk@googlemail.com>
2020-11-27 15:30:16 +0100

    added firebase

--- quasar.conf.js
+++ quasar.conf.js
@@ -18,7 +18,7 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
     boot: [
-      
+
       'axios',
     ],
 
@@ -44,6 +44,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      distDir: 'public',
 
       // Add dependencies for transpiling with Babel (Array of regexes)
       // (from node_modules, which are by default not transpiled).
