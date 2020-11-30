a0513d36b34761ff6d4256f91e469ca53a209d29
Pascal Dziersk <p.dziersk@googlemail.com>
2020-11-30 16:18:38 +0100

    fixed relativ path

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,12 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('../layouts/MainLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/PageCalendar.vue') },
-      { path: '/shoppinglist', component: () => import('pages/PageShoppinglist.vue') },
-      { path: '/changemeal', component: () => import('pages/PageChangeMeal.vue') },
-      { path: '/profile', component: () => import('pages/PageProfile.vue') }
+      { path: '/', component: () => import('../pages/PageCalendar.vue') },
+      { path: '/shoppinglist', component: () => import('../pages/PageShoppinglist.vue') },
+      { path: '/changemeal', component: () => import('../pages/PageChangeMeal.vue') },
+      { path: '/profile', component: () => import('../pages/PageProfile.vue') }
     ]
   }
 ]
@@ -15,7 +15,7 @@ const routes = [
 // Always leave this as last one
 routes.push({
   path: '*',
-  component: () => import('pages/Error404.vue')
+  component: () => import('../pages/Error404.vue')
 })
 
 export default routes
