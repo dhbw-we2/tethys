4ee640e779a62cad60f800bc573117901d9f1295
Robin <Robin@Robin.Robin>
2020-12-20 19:57:17 +0100

    ChangeMeal: Liste der Meals Hinzufügemöglichkeiten Editiermöglichkeiten

--- src/pages/PageChangeMeal.vue
+++ src/pages/PageChangeMeal.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex-center items-baseline">
 
-    <!-- Zutaten vorschlagen, Bevor ZUtat hinzugefügt wírd prüfen, Design, Dokumentation
+    <!-- Design, Dokumentation
 
     Ladebalken -->
 
@@ -55,7 +55,7 @@
                v-bind:key="counter">
             Zutat {{counter}}:<br>
             <!-- DisplayName:--> {{ ingredients.DisplayName }}<br>
-            Menge: {{ ingredients.Gramm }}g<br>
+            Menge: {{ ingredients.Amount }}g<br>
             KalorienPer100g: {{ ingredients.CaloriesPer100g }}
             <!-- id: {{zutat.id}} -->
           </div><br>
@@ -114,6 +114,7 @@
 
 
             <q-select
+              required
               filled
               use-input
               label-color="green"
@@ -123,7 +124,7 @@
               label="Zutat"
               :options="options"
               :option-value = "opt => opt.DisplayName"
-              :option-label = "opt => opt.DisplayName + ' (' + opt.CaloriesPer100g + 'kcal)'"
+              :option-label = "(opt) => {if(opt.DisplayName !== undefined){return opt.DisplayName + ' (' + opt.CaloriesPer100g + 'kcal)'}}"
               @input="(opt) => { SelectOptionToIngredient(ingredient,opt); }"
               @filter="filterFn"
               @filter-abort="abortFilterFn"
@@ -138,23 +139,9 @@
               </template>
             </q-select>
 
-
-            <q-input
-              required
-              filled
-              min="0"
-              type="number"
-              label-color="green"
-              v-model="ingredient.CaloriesPer100g"
-              label="Calories per 100g *"
-              lazy-rules
-              :rules="[
-              val => val !== null && val !== '' || 'Bitte Kalorien angeben'
-              ]"/>
-
             <q-btn label="*Neu*"
                    color="primary"
-                   @click="AddIngredient" />
+                   @click="AddNewIngredient" />
 
             <q-input
               required
@@ -162,29 +149,18 @@
               min="0"
               label-color="green"
               type="number"
-              v-model="ingredient.Gramm"
-              label="Menge *"
+              v-model="ingredient.Amount"
+              label="Menge in Gramm"
               lazy-rules
               :rules="[
               val => val !== null && val !== '' || 'Bitte Menge angeben'
               ]"/>
 
-
-
-           <!-- <q-input
-              filled
-              label-color="green"
-              label="Neue Zutat *"
-              v-model="ingredient.DisplayName"
-              required
-              autocomplete="on"
-              filled /> -->
-
           </div>
 
           <q-btn label="+ Zutat "
                  color="primary"
-                 @click="AddIngredient" />
+                 @click="AddEmptyIngredientToLocalList" />
 
 
         </q-card-section>
@@ -194,6 +170,49 @@
           <q-btn flat label="Entfernen" color="primary" @click="DialogAdditRemoveMeal()" v-close-popup />
           <q-btn flat type="submit" label="Speichern" color="primary" @click="DialogAdditSaveToDatabase()" />
         </q-card-actions>
+
+        </q-form>
+      </q-card>
+    </q-dialog>
+
+    <!-- New INGREDIENT DIALOG BOX-->
+    <q-dialog v-model="DialogNewIngredient">
+
+      <q-card>
+        <q-form ref="NewIngredientForm" class="q-gutter-md">
+
+          <q-card-section class="row items-center q-mx-xl">
+            Neue Zutat hinzufügen:
+            <div class="NewIngredientContainer"
+                 v-model="DialogNewIngredientObject">
+
+              <q-input
+                class="DialogInputTextColor"
+                required
+                filled
+                label-color="green"
+                v-model="DialogNewIngredientObject.DisplayName"
+                label="Zutatname"
+                :rules="[
+              val => val !== null && val !== '']"
+              />
+
+              <q-input
+                filled
+                min="0"
+                type="number"
+                label-color="green"
+                v-model="DialogNewIngredientObject.CaloriesPer100g"
+                label="Kalories per 100g"/>
+
+            </div>
+
+          </q-card-section>
+
+          <q-card-actions align="right">
+            <q-btn flat label="Schließen" color="primary" v-close-popup />
+            <q-btn flat type="submit" label="Speichern" color="primary" @click="DialogNewIngredientSaveToDatabase()" />
+          </q-card-actions>
 
         </q-form>
       </q-card>
@@ -215,9 +234,11 @@ export default {
       meals:[],
       options: [],
       DialogShowMealObject: { Displayname: "", Preparation:"" ,ImageUrl: "", Time: 0, Calorie: 0, Id: 0 }, // Backing Field for Show Meal Dialog
-      DialogShowIngredientList:[{ Name: '', Gramm:'', Id:0}],
+      DialogNewIngredientObject: { Displayname: "",  Calorie: 0 },
+      DialogShowIngredientList:[{ Name: '', Amount:'', Id:0}],
       DialogShowMeal: false, // Visibility boolean for Show Meal Dialog
       DialogAdditMeal: false, // Visibility boolean for Add/Edit Meal Dialog
+      DialogNewIngredient: false, // Visibility boolean for New Ingredient Dialog,
 
     }
   },
@@ -269,11 +290,16 @@ export default {
       this.loadIntegredientOptions();
     },
 
-    AddIngredient() {
+    AddEmptyIngredientToLocalList() {
       this.DialogShowIngredientList.push({
         Name: '',
         Menge: ''
       })
+    },
+
+    AddNewIngredient() {
+      this.DialogNewIngredientObject = { Displayname: "",  Calorie: 0 },
+      this.DialogNewIngredient = true;
     },
 
     CloseNewIngredient(counter) {
@@ -327,26 +353,17 @@ export default {
       if(isValidated === true)
       {
         for (let i = 0; i < this.DialogShowIngredientList.length; i++) {
-          // TODO lookup for this Zutat
 
-          let gramm = this.DialogShowIngredientList[i].Gramm;
-          if(this.DialogShowIngredientList[i].DisplayName !== "") {
-            //TODO add zutat if not available
+          if(this.DialogShowIngredientList[i].DisplayName !== "" && this.DialogShowIngredientList[i].DisplayName !== undefined) {
+            console.log(this.DialogShowIngredientList[i].DisplayName)
+            let createNewEntry = true;
+            let amount = this.DialogShowIngredientList[i].Amount;
 
-            if (this.DialogShowIngredientList[i].Id == 0) {
-              await db.collection("Zutaten").add({
-                DisplayName: this.DialogShowIngredientList[i].DisplayName,
-                CaloriesPer100g: parseInt(this.DialogShowIngredientList[i].CaloriesPer100g)
-              })
-                .then(function (docRef) {
-                  list.push({Path: db.collection('Zutaten').doc(docRef.id), Gramm: parseInt(gramm)});
-                })
-                .catch(function (error) {
-                  console.error("(Zutat)Error adding document: ", error);
-                });
+            let ingredientsQueriedRef = await db.collection("Zutaten").where("DisplayName", "==", this.DialogShowIngredientList[i].DisplayName).where("CaloriesPer100g", "==", this.DialogShowIngredientList[i].CaloriesPer100g).get();
+            if (ingredientsQueriedRef.size >= 0) {
+              console.log(ingredientsQueriedRef.docs[0].id);
+               list.push({Path: db.collection('Zutaten').doc(ingredientsQueriedRef.docs[0].id), Gramm: parseInt(amount)});
             }
-
-
           }
         };
 
@@ -389,9 +406,26 @@ export default {
       }
     },
 
-    DialogAdditRemoveMeal() {
+    async DialogNewIngredientSaveToDatabase()
+    {
+      await db.collection("Zutaten").add({
+        DisplayName: this.DialogNewIngredientObject.DisplayName,
+        CaloriesPer100g: parseInt(this.DialogNewIngredientObject.CaloriesPer100g)
+      })
+        .then(function (docRef) {
+
+        })
+        .catch(function (error) {
+          console.error("(Zutat)Error adding document: ", error);
+        });
+
+      await this.loadIntegredientOptions();
+      this.DialogNewIngredient = false;
+    },
+
+    async DialogAdditRemoveMeal() {
       if(this.DialogShowMealObject.Id != 0)
-        db.collection("Rezepte").doc(this.DialogShowMealObject.Id).delete();
+        await db.collection("Rezepte").doc(this.DialogShowMealObject.Id).delete();
 
       this.LoadMeals();
     },
@@ -414,7 +448,7 @@ export default {
               DisplayName: ingredientObj.data().DisplayName,
               CaloriesPer100g: ingredientObj.data().CaloriesPer100g,
               id: ingredientObj.id,
-              Gramm: ingredientRef.Gramm,
+              Amount: ingredientRef.Gramm,
             })
           })
         })
@@ -440,6 +474,7 @@ export default {
 }
 .Meallistmeal:hover {
   background-color: greenyellow;
+  cursor: pointer;
 }
 
 .NewIngredientContainer{
@@ -452,6 +487,9 @@ export default {
   width: 30px;
   float: right;
   cursor: pointer;
+  color: red;
+  text-align: center;
+  border: 1.5px solid black;
 }
 
 .DialogInputTextColor{
